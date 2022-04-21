7f5f4a92f2e7045174528cf153b761f331e57141
vickybieghsPXL <43740667+vickybieghsPXL@users.noreply.github.com>
2022-04-21 13:21:02 +0200

    finetune

--- scroll.js
+++ scroll.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('html, body, *').mousewheel(function(e, delta) {
-        this.scrollLeft -= (delta);
+        this.scrollLeft -= (delta * 4);
         e.preventDefault();
     });
 });
