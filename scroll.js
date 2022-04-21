9457508cfdcbbf532d21dc8e4e025046a8da85bd
vickybieghsPXL <43740667+vickybieghsPXL@users.noreply.github.com>
2022-04-21 14:55:11 +0200

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,11 +1,6 @@
-
-const element = document.querySelector('body, html');
-
-element.addEventListener('wheel', (event) => {
-    event.preventDefault();
-
-    element.scrollBy({
-        left: event.deltaY < 0 ? -30 : 30,
-
+$(document).ready(function() {
+    $('html, body, *').mousewheel(function(e, delta) {
+        this.scrollLeft -= (delta * 100);
+        e.preventDefault();
     });
 });
