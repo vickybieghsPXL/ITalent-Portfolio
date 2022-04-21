2e1a6353f39ff902383df2ba118c120ff22cd773
vickybieghsPXL <43740667+vickybieghsPXL@users.noreply.github.com>
2022-04-21 14:46:40 +0200

    scrollfaster

--- scroll.js
+++ scroll.js
@@ -1,6 +1,11 @@
-$(document).ready(function() {
-    $('html, body, *').mousewheel(function(e, delta) {
-        this.scrollLeft -= (delta * 4);
-        e.preventDefault();
+
+const element = document.querySelector('body, html');
+
+element.addEventListener('wheel', (event) => {
+    event.preventDefault();
+
+    element.scrollBy({
+        left: event.deltaY < 0 ? -30 : 30,
+
     });
 });
